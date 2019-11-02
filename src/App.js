ff7538934aa85481d5e793c51792e32d414652af
Cooper Michael <coopermichael@Coopers-MacBook-Pro.local>
2019-11-02 15:14:11 -0700

    Initial commit from Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Citizens Unite</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
